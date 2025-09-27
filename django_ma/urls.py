fc06538a39e997a92661ef502a1c01baedea36fc
hun2up <hun2up@gmail.com>
2025-09-27 16:42:02 +0900

    urls 연결 수정

--- django_ma/urls.py
+++ django_ma/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello.urls')),
+    path('', views.index, name='index'),
     path('board/', include('board.urls')),  # hello 앱의 urls 연결
 ]
