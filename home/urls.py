9c90cb7c21f5d7d70b6f8eae6be9bc16b3591150
hun2up <hun2up@gmail.com>
2025-10-10 18:46:57 +0900

    집에가자

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
