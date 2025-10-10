354cf20f0d2ae6183c3ff012544aaec5762b114d
hun2up <hun2up@gmail.com>
2025-10-10 12:58:26 +0900

    homne

--- web_ma/settings.py
+++ web_ma/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home_ma',
-    'join_ma',
+    'home',
+    'join',
 ]
 
 MIDDLEWARE = [
