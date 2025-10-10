dce7c6e667cc8c24c18ceeb8cc3876e949dbe3e4
hun2up <hun2up@gmail.com>
2025-10-10 17:02:13 +0900

    board create

--- board/views.py
+++ board/views.py
@@ -8,3 +8,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'board/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        if title and content:
+            Post.objects.create(title=title, content=content)
+            return redirect('post_list')
+        else:
+            error = "제목과 내용을 모두 입력하세요."
+            return render(request, 'board/post_create.html', {'error': error})
+
+    return render(request, 'board/post_create.html')
