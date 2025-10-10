7b9e1040d803d83792db15ae787c7b4fdb8301ef
hun2up <hun2up@gmail.com>
2025-10-10 13:54:16 +0900

    db connect test

--- join/urls.py
+++ join/urls.py
@@ -1,7 +1,10 @@
 # join/urls.py
 from django.urls import path
 from . import views
+from .views import db_test_view
 
 urlpatterns = [
-    path('', views.join_form, name='join_form'),  # /join/
+    path('', views.join_form, name='join_form'),
+    path('dbtest/', db_test_view, name='db_test'),  # /join/
 ]
+
--- join/views.py
+++ join/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import JoinInfo
 from .forms import JoinForm
+from django.http import JsonResponse
+from django.db import connection
 
 def join_form(request):
     if request.method == 'POST':
@@ -13,3 +15,12 @@ def join_form(request):
     else:
         form = JoinForm()
     return render(request, 'join/join_form.html', {'form': form})
+
+def db_test_view(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1;")
+            row = cursor.fetchone()
+        return JsonResponse({"db_connection": "ok", "result": row[0]})
+    except Exception as e:
+        return JsonResponse({"db_connection": "error", "message": str(e)})
