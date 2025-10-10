613d0124dcb20402ec2393f1ef4f8e4aa5903ee9
hun2up <hun2up@gmail.com>
2025-10-10 12:36:16 +0900

    sqlite3

--- web_ma/settings.py
+++ web_ma/settings.py
@@ -77,11 +77,10 @@ WSGI_APPLICATION = 'web_ma.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
