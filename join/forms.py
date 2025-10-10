31ebd80466ee91884b52f5fcc87156d42f6a50d8
hun2up <hun2up@gmail.com>
2025-10-10 15:48:14 +0900

    email edit

--- join/forms.py
+++ join/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from .models import JoinInfo
 
 class JoinForm(forms.ModelForm):
+    email = forms.EmailField(required=False)  # ✅ 필수 아님으로 명시
+    
     class Meta:
         model = JoinInfo
         fields = ['name', 'ssn', 'address', 'phone', 'email']
