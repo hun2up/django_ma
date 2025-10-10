e7c28ac7ecb421b3eb1537223db5cf8af35ec3d6
hun2up <hun2up@gmail.com>
2025-10-10 13:32:14 +0900

    .env

--- web_ma/settings.py
+++ web_ma/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'web_ma.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # SQLite 엔진
-        'NAME': BASE_DIR / 'db.sqlite3',         # DB 파일 경로
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",   # 로컬 개발용
+        conn_max_age=600,
+    )
 }
 
 
