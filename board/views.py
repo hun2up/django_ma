25a874186e702a1aeaad37a0d203170a188ca6c7
hun2up <hun2up@gmail.com>
2025-10-10 18:28:25 +0900

    login

--- board/views.py
+++ board/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
+@login_required
 def post_list(request):
     post_list = Post.objects.order_by('-created_at')
     paginator = Paginator(post_list, 10)  # 10개씩
@@ -9,10 +11,12 @@ def post_list(request):
     posts = paginator.get_page(page)
     return render(request, 'board/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'board/post_detail.html', {'post': post})
 
+@login_required
 def post_create(request):
     if request.method == 'POST':
         title = request.POST.get('title')
