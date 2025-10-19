c71b8b8443dbdac1717fb3edbf81fc50ccbf0032
hun2up <hun2up@gmail.com>
2025-10-19 15:59:00 +0900

    finish board

--- board/templatetags/board_filters.py
+++ board/templatetags/board_filters.py
@@ -3,11 +3,6 @@ from django import template
 
 register = template.Library()
 
-print("✅ custom_filters.py 로드됨")  # 서버 실행 시 출력됨
-
 @register.filter
 def basename(value):
-    print(f"📦 basename 필터 호출됨 - 값: {value}")  # 템플릿에서 사용될 때 출력됨
     return os.path.basename(value)
-
-print("✅ custom_filters.py 로드됨")
