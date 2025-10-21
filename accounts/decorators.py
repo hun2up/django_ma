727df4c2dfb98bc3bb8baf83c33b4fe9025d9fbc
hun2up <hun2up@gmail.com>
2025-10-21 18:49:59 +0900

    퇴근하자

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -5,12 +5,10 @@ from django.contrib.auth.decorators import login_required
 
 def grade_required(allowed_grades):
     def decorator(view_func):
-        @wraps(view_func)
         @login_required
         def _wrapped_view(request, *args, **kwargs):
-            user = request.user
-            if getattr(user, 'grade', None) not in allowed_grades:
-                return render(request, 'no_permission_popup.html')
-            return view_func(request, *args, **kwargs)
+            if request.user.grade in allowed_grades:
+                return view_func(request, *args, **kwargs)
+            return render(request, "no_permission_popup.html")
         return _wrapped_view
     return decorator
--- board/urls.py
+++ board/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('create/', views.post_create, name='post_create'),  # 글 작성 페이지
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('support_form/', views.support_form, name='support_from'),
+    path('support_form/', views.support_form, name='support_form'),
     path('support_manual/', views.support_manual, name='support_manual'),
     path('support_rules/', views.support_rules, name='support_rules'),
+    path('support_form/', views.support_form, name='support_form'),
+    path('search-user/', views.search_user, name='search_user'),
+    path('generate-pdf/', views.generate_request_pdf, name='generate_pdf'),
 ]
